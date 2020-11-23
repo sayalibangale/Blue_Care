3f8a8bab8e87a7be87a85289553b21a71f23ad09
harshitajain1811 <56118132+harshitajain1811@users.noreply.github.com>
2020-11-23 20:50:30 +0530

    Update beachname_details.php

--- wdl_project/beachname_details.php
+++ wdl_project/beachname_details.php
@@ -1,21 +1,21 @@
 <?php
+
 $conn = mysqli_connect("localhost:3307","root","root","wdl_project");
 
-$postDetails = array();
-
-$search_key = $_GET['term'];
-
-//get rows query
-$query = "SELECT * FROM beach where bname like '%".$search_key."%'";
-$result = mysqli_query($conn, $query);
-
-//number of rows
-$rowCount = mysqli_num_rows($result);
-
-if($rowCount > 0){
-    while($row = mysqli_fetch_assoc($result)){
-			$postDetails[] = $row['bname'];
-	}
+if (isset($_GET['term'])) {
+     
+   $query = "SELECT * FROM beach WHERE bname LIKE '{$_GET['term']}%' LIMIT 25";
+    $result = mysqli_query($conn, $query);
+ 
+    if (mysqli_num_rows($result) > 0) {
+     while ($user = mysqli_fetch_array($result)) {
+      $res[] = $user['bname'];
+     }
+    } else {
+      $res = array();
+    }
+    //return json res
+    echo json_encode($res);
 }
-echo json_encode($postDetails);
+
 ?>
